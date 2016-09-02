63967fa815fbd61d75a228999d16fb8aa466adb4
Joe Martell <joseph.martell@gmail.com>
2016-09-02 15:37:52 -0700

    First-pass definition of a profile.

---
+++ SetIPLib/Profile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace SetIPLib {
+
+    public class Profile {
+        private string _name;
+
+        public string Name {
+            get { return _name; }
+        }
+
+        private IPAddress _ip;
+
+        public IPAddress IP {
+            get { return _ip; }
+        }
+
+        private IPAddress _subnet;
+
+        public IPAddress Subnet {
+            get { return _subnet; }
+        }
+
+        public Profile(string name, IPAddress ip, IPAddress subnet) {
+            _name = name;
+            _ip = ip;
+            _subnet = subnet;
+        }
+    }
+}
