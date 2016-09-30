c9fd68651324bcd83a1203a48884e2b586088d91
TheLoudestNinja <joseph.martell@gmail.com>
2016-09-30 08:41:34 -0700

    Somehow the new files and the renamed AddProfile.cs file got missed in my last commit.  Adding files to repository and committing.

---
+++ SetIPCLI/CLIAddProfile.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetIPLib;
+using System.Net;
+
+namespace SetIPCLI {
+
+    /// <summary>
+    /// Expected CLI syntax:
+    ///   -a "Profile name" dhcp
+    ///   -add "Other name" static 192.168.1.1 255.255.255.0
+    /// </summary>
+    class CLIAddProfile : ICLICommand {
+        private enum ExpectedParameter {
+            Name,
+            Source,
+            IP,
+            Sub,
+            None
+        }
+
+        /// <summary>
+        /// Arguments that will be parsed to generate a new profile.
+        /// </summary>
+        public ArgumentGroup Arguments { get; }
+
+        public CLIAddProfile(ArgumentGroup args) {
+            Arguments = args;
+        }
+
+
+        public void Execute(ref IProfileStore store) {
+            var currentProfiles = store?.Retrieve().ToList();
+            ExpectedParameter nextParm = ExpectedParameter.Name;
+
+            string name = string.Empty;
+            bool UseDHCP = true;
+            IPAddress ip = IPAddress.Any;
+            IPAddress sub = IPAddress.Any;
+
+            foreach (var parm in Arguments.Arguments) {
+                switch (nextParm) {
+                    case ExpectedParameter.Name:
+                        name = parm;
+                        nextParm = ExpectedParameter.Source;
+                        break;
+                    case ExpectedParameter.Source:
+                        if (parm.ToUpper() == "DHCP") {
+                            UseDHCP = true;
+                            nextParm = ExpectedParameter.None;
+                        }
+                        else {
+                            UseDHCP = false;
+                            ip = IPAddress.Parse(parm);
+                            nextParm = ExpectedParameter.Sub;
+                        }
+                        break;
+                    case ExpectedParameter.IP:
+                        ip = IPAddress.Parse(parm);
+                        nextParm = ExpectedParameter.Sub;
+                        break;
+                    case ExpectedParameter.Sub:
+                        sub = IPAddress.Parse(parm);
+                        nextParm = ExpectedParameter.None;
+                        break;
+                    case ExpectedParameter.None:
+                        break;
+                }
+                if (nextParm == ExpectedParameter.None) {
+                    break;
+                }
+            }
+            if (UseDHCP) {
+                currentProfiles?.Add(new Profile(name));
+            }
+            else {
+                currentProfiles?.Add(new Profile(name, ip, sub));
+            }
+
+            store?.Store(currentProfiles);
+
+        }
+    }
+}
---
+++ SetIPCLI/CLIUseProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetIPLib;
+
+namespace SetIPCLI {
+    class CLIUseProfile : ICLICommand {
+
+        public ArgumentGroup Arguments { get; }
+
+        public void Execute(ref IProfileStore store) {
+            string profileName = Arguments.Arguments.DefaultIfEmpty(string.Empty).FirstOrDefault();
+            var profiles = store.Retrieve();
+
+            //If the supplied profile name is not found, DHCP is used instead.
+            Profile target = (from p in profiles
+                             where p.Name == profileName
+                             select p).DefaultIfEmpty(Profile.DHCPDefault).First();
+
+            //Eventually, the adapter name will be one of the arguments that can be passed.
+            ProfileApplier.ApplyProfile("Local Area Connection", target);
+        }
+
+        public CLIUseProfile(ArgumentGroup args) {
+            Arguments = args;
+        }
+    }
+}
