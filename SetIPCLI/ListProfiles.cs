34017e93873a0e5fb0545df2af5719268482eedf
josephmartell <joseph.martell@gmail.com>
2021-02-25 18:29:38 -0800

    Migrate to CLImber
    This commit removes the original CLI code and migrates all commands
    over to CLImber. All existing commands, with the exception of
    edit profile, have been migrated and are functional. Unit tests
    have been added for commands that can be tested. Some commands
    do not have tests, like list, but these commands are basically
    informational so unit tests aren't being considered critical right now.
    
    Resolves: #16, #15

---
+++ SetIPCLI/ListProfiles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLImber;
+using SetIPLib;
+
+namespace SetIPCLI
+{
+    [CommandClass("list")]
+    public class ListProfiles
+    {
+        public IProfileStore Store { get; }
+        public ListProfiles(IProfileStore store)
+        {
+            Store = store;
+        }
+
+        [CommandHandler]
+        public void ListAllProfiles()
+        {
+            IEnumerable<Profile> profiles = Store.Retrieve();
+
+            foreach (var profile in profiles.OrderBy(p => p.Name))
+            {
+                PrintProfile(profile);
+            }
+
+        }
+
+        public void ShowFilteredProfileList(string filter)
+        {
+            IEnumerable<Profile> profiles = Store.Retrieve().Where((p, b) => p.Name.ToUpper().Contains(filter.ToUpper()));
+            foreach (var profile in profiles.OrderBy(p => p.Name))
+            {
+                PrintProfile(profile);
+            }
+
+        }
+
+        private void PrintProfile(Profile profile)
+        {
+            if (profile.UseDHCP)
+            {
+                Console.WriteLine("{0,-35} DHCP", profile.Name);
+            }
+            else
+            {
+                Console.WriteLine("{0,-30} {1,-15} {2,-15} {3,-15}", profile.Name, profile.IP.ToString(), profile.Subnet.ToString(), profile.Gateway.ToString());
+            }
+        }
+
+    }
+}
